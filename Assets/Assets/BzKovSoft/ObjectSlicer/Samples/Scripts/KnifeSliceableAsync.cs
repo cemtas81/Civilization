ef372007b04555d465c7a7611aa32fa6c7af4c55
cemtas81 <79138234+cemtas81@users.noreply.github.com>
2023-06-23 10:27:27 +0300

    bzknife errors fixed throw particle color change

--- Assets/Assets/BzKovSoft/ObjectSlicer/Samples/Scripts/KnifeSliceableAsync.cs
+++ Assets/Assets/BzKovSoft/ObjectSlicer/Samples/Scripts/KnifeSliceableAsync.cs
@@ -32,22 +32,23 @@ namespace BzKovSoft.ObjectSlicer.Samples
 			// The call from OnTriggerEnter, so some object positions are wrong.
 			// We have to wait for next frame to work with correct values
 			yield return null;
-
-			Vector3 point = GetCollisionPoint(knife);
-			Vector3 normal = Vector3.Cross(knife.MoveDirection, knife.BladeDirection);
-			Plane plane = new Plane(normal, point);
-
-			if (_sliceableAsync != null)
+			if (knife!=null)
 			{
-				_sliceableAsync.Slice(plane, knife.SliceID, null);
-			}
+                Vector3 point = GetCollisionPoint(knife);
+                Vector3 normal = Vector3.Cross(knife.MoveDirection, knife.BladeDirection);
+                Plane plane = new Plane(normal, point);
+                if (_sliceableAsync != null)
+                {
+                    _sliceableAsync.Slice(plane, knife.SliceID, null);
+                }
+            }
+
 		}
 
 		private Vector3 GetCollisionPoint(BzKnife knife)
 		{
 			Vector3 distToObject = transform.position - knife.Origin;
 			Vector3 proj = Vector3.Project(distToObject, knife.BladeDirection);
-
 			Vector3 collisionPoint = knife.Origin + proj;
 			return collisionPoint;
 		}
