04db3b21b275b058668ff257c13ad796814a9fdf
cem <cemtas81@gmail.com>
2023-07-10 22:13:32 +0300

    footstep shortened raycast

--- Assets/Assets/Scripts/FootprintController.cs
+++ Assets/Assets/Scripts/FootprintController.cs
@@ -3,22 +3,22 @@ using UnityEngine;
 
 public class FootprintController : MonoBehaviour
 {
-    private ObjectPooling2 objectPooling; // Reference to the ObjectPooling2 script
+    private ObjectPooling2 objectPooling; 
     public Transform leftFootPos;
     public Transform rightFootPos;
     public float footprintOffset = 0.05f; // Offset value to raise the footprint above the hit point
 
     private void Start()
     {
-        objectPooling = ObjectPooling2.SharedInstance; // Get a reference to the ObjectPooling2 instance
+        objectPooling = ObjectPooling2.SharedInstance; 
     }
 
     void LeftPrint()
     {
         RaycastHit hit;
-        if (Physics.Raycast(leftFootPos.position, leftFootPos.forward, out hit))
+        if (Physics.Raycast(leftFootPos.position, leftFootPos.forward, out hit,0.2f))
         {
-            GameObject footprintPrefab = objectPooling.GetPooledObject(objectPooling.objectsToPool[0]); // Get a pooled footstep prefab
+            GameObject footprintPrefab = objectPooling.GetPooledObject(objectPooling.objectsToPool[0]); 
 
             if (footprintPrefab != null)
             {
@@ -26,7 +26,6 @@ public class FootprintController : MonoBehaviour
                 footprintPrefab.transform.SetPositionAndRotation(footprintPosition, Quaternion.LookRotation(hit.normal, leftFootPos.up));
                 footprintPrefab.SetActive(true);
 
-                // Deactivate the footstep prefab after a delay
                 StartCoroutine(DeactivateFootstepDelayed(footprintPrefab));
             }
         }
@@ -35,17 +34,15 @@ public class FootprintController : MonoBehaviour
     void RightPrint()
     {
         RaycastHit hit;
-        if (Physics.Raycast(rightFootPos.position, rightFootPos.forward, out hit))
+        if (Physics.Raycast(rightFootPos.position, rightFootPos.forward, out hit,0.2f))
         {
-            GameObject footprintPrefab = objectPooling.GetPooledObject(objectPooling.objectsToPool[1]); // Get a pooled footstep prefab
-
+            GameObject footprintPrefab = objectPooling.GetPooledObject(objectPooling.objectsToPool[1]); 
             if (footprintPrefab != null)
             {
                 Vector3 footprintPosition = hit.point + hit.normal * footprintOffset; // Calculate the footprint position with an offset
                 footprintPrefab.transform.SetPositionAndRotation(footprintPosition, Quaternion.LookRotation(hit.normal, rightFootPos.up));
                 footprintPrefab.SetActive(true);
 
-                // Deactivate the footstep prefab after a delay
                 StartCoroutine(DeactivateFootstepDelayed(footprintPrefab));
             }
         }
@@ -53,7 +50,7 @@ public class FootprintController : MonoBehaviour
 
     private IEnumerator DeactivateFootstepDelayed(GameObject footprintPrefab)
     {
-        yield return new WaitForSeconds(1f); // Change the delay as per your requirement
+        yield return new WaitForSeconds(1f); 
         footprintPrefab.SetActive(false);
     }
 }
