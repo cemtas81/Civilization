66256f123199940d215e3754b958b3732258a907
cemtas81 <79138234+cemtas81@users.noreply.github.com>
2023-07-10 14:52:37 +0300

    fixed footsteps

--- Assets/Assets/Scripts/FootprintController.cs
+++ Assets/Assets/Scripts/FootprintController.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 public class FootprintController : MonoBehaviour
 {
     private ObjectPooling2 objectPooling; // Reference to the ObjectPooling2 script
+    public Transform leftFootPos;
+    public Transform rightFootPos;
+    public float footprintOffset = 0.05f; // Offset value to raise the footprint above the hit point
 
     private void Start()
     {
         objectPooling = ObjectPooling2.SharedInstance; // Get a reference to the ObjectPooling2 instance
     }
 
-    private void OnTriggerEnter(Collider other)
+    void LeftPrint()
     {
-        if (other.CompareTag("Finish"))
+        RaycastHit hit;
+        if (Physics.Raycast(leftFootPos.position, leftFootPos.forward, out hit))
         {
-            Vector3 position = other.ClosestPoint(transform.position);
-            Quaternion rotation = Quaternion.LookRotation(other.transform.up, transform.up);
-            position += Vector3.up * 0.01f;
             GameObject footprintPrefab = objectPooling.GetPooledObject(objectPooling.objectsToPool[0]); // Get a pooled footstep prefab
 
             if (footprintPrefab != null)
             {
-                footprintPrefab.transform.SetPositionAndRotation(position, rotation);
+                Vector3 footprintPosition = hit.point + hit.normal * footprintOffset; // Calculate the footprint position with an offset
+                footprintPrefab.transform.SetPositionAndRotation(footprintPosition, Quaternion.LookRotation(hit.normal, leftFootPos.up));
+                footprintPrefab.SetActive(true);
+
+                // Deactivate the footstep prefab after a delay
+                StartCoroutine(DeactivateFootstepDelayed(footprintPrefab));
+            }
+        }
+    }
+
+    void RightPrint()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(rightFootPos.position, rightFootPos.forward, out hit))
+        {
+            GameObject footprintPrefab = objectPooling.GetPooledObject(objectPooling.objectsToPool[1]); // Get a pooled footstep prefab
+
+            if (footprintPrefab != null)
+            {
+                Vector3 footprintPosition = hit.point + hit.normal * footprintOffset; // Calculate the footprint position with an offset
+                footprintPrefab.transform.SetPositionAndRotation(footprintPosition, Quaternion.LookRotation(hit.normal, rightFootPos.up));
                 footprintPrefab.SetActive(true);
 
                 // Deactivate the footstep prefab after a delay
