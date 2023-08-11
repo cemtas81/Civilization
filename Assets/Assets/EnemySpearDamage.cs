57558c5160d2367d50839e8193b91a48aa8bf909
cemtas81 <79138234+cemtas81@users.noreply.github.com>
2023-08-11 15:23:06 +0300

    Patlama effect/ axe - spear bump
    better agac and stone with albedo alpha shader

--- Assets/Assets/EnemySpearDamage.cs
+++ Assets/Assets/EnemySpearDamage.cs
@@ -6,7 +6,7 @@ public class EnemySpearDamage : MonoBehaviour
  
     private Vector3 targetPosition;
     private Rigidbody rb;
-
+    public GameObject dust;
     private void Start()
     {
   
@@ -16,20 +16,25 @@ public class EnemySpearDamage : MonoBehaviour
         rb.isKinematic = true;     
         float distance = Vector3.Distance(transform.position, targetPosition);
         targetPosition.y = 1;
-        float duration = distance / 12;
+        float duration = distance / 10;
         transform.DOMove(targetPosition, duration).SetEase(Ease.Linear)
             .OnComplete(() => rb.isKinematic=false);
     }
 
     void OnTriggerEnter(Collider other)
     {
-   
+        Quaternion rotation = Quaternion.LookRotation(-transform.forward);
         if (other.CompareTag("Player"))
         {
             BarbCont2 enemy = other.GetComponent<BarbCont2>();
             enemy.LoseHealth(5);
             Dest();
-        }       
+        }
+        else if (other.CompareTag("Sword"))
+        {
+            Instantiate(dust,transform.position,rotation);
+            Dest();
+        }
         //else
         //{
         //    GetComponent<Collider>().isTrigger = false; 
